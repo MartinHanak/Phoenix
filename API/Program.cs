5facefb907d5c3d23e0fb4e6b0407cc185109138
Martin Hanák <martinhanak97@gmail.com>
2023-10-06 13:39:06 +0200

    Group account endpoints into /account

--- API/Program.cs
+++ API/Program.cs
@@ -40,7 +40,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapIdentityApi<IdentityUser>();
+app.MapGroup("/account").MapIdentityApi<IdentityUser>();
 
 app.UseHttpsRedirection();
 
